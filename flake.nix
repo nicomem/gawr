0a7f8b4192047fa2c3a90f889f8320ca2f597e40
Nicolas Mémeint <nicomem@pm.me>
2023-02-08 22:57:06 +0100

    Add nixpkgs as nix flake input

--- flake.nix
+++ flake.nix
@@ -1,5 +1,11 @@
 {
-  inputs.dream2nix.url = "github:nix-community/dream2nix";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    dream2nix.url = "github:nix-community/dream2nix";
+    dream2nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
   outputs = inp:
     inp.dream2nix.lib.makeFlakeOutputs {
       systems = ["x86_64-linux"];
